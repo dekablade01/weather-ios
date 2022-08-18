b03841d1ce9755b2a41ec14651b62e2a29a6deda
Issarapong Poesua <dekablade01@gmail.com>
2022-08-18 20:21:39 +0700

    Initial Commit

---
+++ Shared/KrungsriAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  KrungsriAssignmentApp.swift
+//  Shared
+//
+//  Created by Issarapong Poesua on 18/8/22.
+//
+
+import SwiftUI
+
+@main
+struct KrungsriAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
